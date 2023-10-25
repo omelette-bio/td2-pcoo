82cf2fee62d3b29f48c1ee9ef555caf125655190
omelette-bio <omelettebio@proton.me>
2023-10-25 12:39:17 +0200

    modified:   source/Main.java 	deleted:    source/Message.java 	modified:   source/Midi.java

--- source/Main.java
+++ source/Main.java
@@ -4,7 +4,8 @@ public class Main {
   public static void main(String[] args) {
     // open a midi file to read the content, not play the music
     try {
-      Midi midi_file = new Midi(args[0]);
+      Midi midi_file = new Midi();
+      midi_file.parseMidi(args[0]);
       midi_file.afficherMidi();
     } catch (Exception e) {
       e.printStackTrace();
--- source/Midi.java
+++ source/Midi.java
@@ -8,12 +8,34 @@ public class Midi {
   protected int tempo = 120;
   protected ArrayList<Message> messages = new ArrayList<Message>();
 
-  Midi (String file) throws Error, InvalidMidiDataException, IOException {
-    this.sequence = MidiSystem.getSequence(new File(file));
+  private class Message {
+    ShortMessage sm = null; // Initialize the message to null
+    int channel = 0;
+    long tick = 0;
+    int key = 0;
+    int octave = 0;
+    int note = 0;
+    int volume = 0;
+    int cmd = 0;
+  
+    public void infosMessage(MidiEvent event) throws InvalidMidiDataException {
+      this.sm = (ShortMessage) event.getMessage();
+      this.channel = sm.getChannel();
+      this.tick = event.getTick();
+      this.key = sm.getData1();
+      this.octave = (key / 12) - 1;
+      this.note = key % 12;
+      this.volume = sm.getData2();
+      this.cmd = sm.getCommand();
+    }
+  }
+
+  public void parseMidi(String midiFile)  throws Error, InvalidMidiDataException, IOException {
+    this.sequence = MidiSystem.getSequence(new File(midiFile));
     for (Track track : sequence.getTracks()) {
       for (int i = 0; i < track.size(); i++) {
         if (track.get(i).getMessage() instanceof MetaMessage) {
-          this.getTempoMorceau(track.get(i));
+          this.setTempoMorceau(track.get(i));
         } else {
           this.addMessage(track.get(i));
         }
@@ -21,7 +43,7 @@ public class Midi {
     }
   }
 
-  public void getTempoMorceau(MidiEvent event) {
+  public void setTempoMorceau(MidiEvent event) {
     MetaMessage mm = (MetaMessage) event.getMessage();
     byte[] msg = mm.getMessage();
     if (((msg[1] & 0xFF) == 0x51) && (msg[2] == 0x03)) {
