8414d05bec81b5497ab4f1801e8702ea3e65e218
omelette-bio <omelettebio@proton.me>
2023-10-25 12:43:15 +0200

    modified:   source/Main.java

--- source/Main.java
+++ source/Main.java
@@ -1,14 +1,15 @@
-// import javax.sound.midi.*;
+import javax.sound.midi.InvalidMidiDataException;
 
 public class Main {
   public static void main(String[] args) {
-    // open a midi file to read the content, not play the music
     try {
       Midi midi_file = new Midi();
       midi_file.parseMidi(args[0]);
       midi_file.afficherMidi();
+    } catch (InvalidMidiDataException e) {
+      System.err.println("error in the midi file");
     } catch (Exception e) {
-      e.printStackTrace();
+      System.err.println("an error has occured");
     }
   }
 }
