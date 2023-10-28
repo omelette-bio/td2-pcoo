1ad7b1c1e047a26d3fa9137d6c412d0982d44ff3
omelette-bio <omelettebio@proton.me>
2023-10-29 00:43:30 +0200

    modified:   source/Midi.java

--- source/Midi.java
+++ source/Midi.java
@@ -66,6 +66,5 @@ public class Midi {
     for (Message message : this.messages) {
       System.out.println("Channel: " + message.channel + " Tick: " + message.tick + " Key: " + message.key + " Octave: " + message.octave + " Note: " + message.note + " Volume: " + message.volume + " Command: " + message.cmd);
     }
-    // System.out.println("Channel: " + messages.get(0).channel + " Tick: " + messages.get(0).tick + " Key: " + messages.get(0).key + " Octave: " + messages.get(0).octave + " Note: " + messages.get(0).note + " Volume: " + messages.get(0).volume + " Command: " + messages.get(0).cmd);
   }
 }
